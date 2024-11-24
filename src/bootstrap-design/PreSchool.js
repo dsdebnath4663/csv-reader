a4190e078ed8883698e38f11014af97e5a80a717
debnath <dsdebnath4663@gmail.com>
2024-11-24 12:40:16 +0530

    Become A Teacher Section

--- src/bootstrap-design/PreSchool.js
+++ src/bootstrap-design/PreSchool.js
@@ -355,24 +355,33 @@ function PreSchool() {
       </div>
 
       {/* Become A Teacher Section*/}
-      <div className=" container-xxl py-5">
-        <div className="container bg-white mt-5">
-          <div class="row justify-content-center align-items-center g-2">
-            {/* Become A Teacher Left side image Section*/}
-            <div class="col-md-6 col-md-teacher">
-              left
-              <div class="position-relative ">
-                <img class="position-absolute w-100  rounded col-md-teacher-img" src="https://themewagon.github.io/kider/img/call-to-action.jpg" />
+      <div class="container-xxl py-5">
+        <div class="container">
+          <div class="bg-orange rounded">
+            <div class="row g-0">
+              <div class="col-lg-6"  >
+                {/* style="min-height: 400px; visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;"> */}
+                <div class="position-relative h-100">
+                  <img class="position-absolute w-100 h-100 rounded" src="https://themewagon.github.io/kider/img/call-to-action.jpg" />
+                  {/* // style="object-fit: cover;" /> */}
+                </div>
+              </div>
+              <div class="col-lg-6 ">
+                <div class="h-100 d-flex flex-column justify-content-center p-5">
+                  <h1 class="mb-4">Become A Teacher</h1>
+                  <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos.
+                    Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet
+                  </p>
+                  <a class="btn btn-primary py-3 px-5" href="">Get Started Now<i class="fa fa-arrow-right ms-2"></i></a>
+                </div>
               </div>
             </div>
-
-            <div class="col-md-6">Right</div>
           </div>
         </div>
       </div>
 
 
-    </div>
+    </div >
   );
 }
 
